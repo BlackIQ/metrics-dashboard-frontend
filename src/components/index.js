f6876df372615190f7b8a3f8e1f209cde1aa755a
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2024-12-14 21:04:01 +0330

    Add components. Confirm, Form, Loading

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Loading from "@/components/loading/loading.component";
+import Confirm from "@/components/confirm/confirm.component";
+import Form from "@/components/form/form.component";
+
+export { Loading, Confirm, Form };
