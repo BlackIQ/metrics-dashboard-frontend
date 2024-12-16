50c4ed6bff936db721fbd8a6702c42631fb48055
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2024-12-16 11:47:20 +0330

    New component. Toast

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Loading from "@/components/loading/loading.component";
 import Confirm from "@/components/confirm/confirm.component";
+import Toast from "@/components/toast/toast.component";
 import Form from "@/components/form/form.component";
 
-export { Loading, Confirm, Form };
+export { Loading, Confirm, Form, Toast };
