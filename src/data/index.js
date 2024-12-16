1310618b8f1fc36017de1e91fb425ddd5e102638
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2024-12-16 18:39:25 +0330

    Ok

--- src/data/index.js
+++ src/data/index.js
@@ -1,1 +1,12 @@
 export const version = "1.0.0";
+
+export const communicationTypes = [
+  {
+    label: "IP",
+    value: true,
+  },
+  {
+    label: "DNS",
+    value: false,
+  },
+];
---
+++ src/pages/panel/hosts/index.js
@@ -0,0 +1,125 @@
+// NextJS ReactJs
+import { useState, useEffect } from "react";
+import Head from "next/head";
+
+// Material UI
+import { Box, Dialog, DialogContent, DialogTitle } from "@mui/material";
+
+// Components
+import { Table, Loading, Confirm } from "@/components";
+
+// Hooks
+import { useDisclosure, useToast } from "@/hooks";
+
+// APIs
+import { all as allHosts, deleteOne as deleteHost } from "@/api/services/host";
+
+// Forms
+import HostForm from "@/forms/host";
+
+const Index = () => {
+  const [hosts, setHosts] = useState([]);
+
+  const [loading, setLoading] = useState(true);
+  const [currentData, setCurrentData] = useState({});
+
+  const { isOpen: confirmOpen, onToggle: handleConfirm } = useDisclosure();
+  const { isOpen: dialogOpen, onToggle: handleDialog } = useDisclosure();
+
+  const toast = useToast();
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    setLoading(true);
+
+    try {
+      const { hosts } = await allHosts();
+
+      setHosts(hosts);
+
+      toast("Hosts got");
+    } catch (error) {
+      toast(error.message);
+    }
+
+    setLoading(false);
+  };
+
+  const deleteData = async () => {
+    setLoading(true);
+
+    try {
+      await deleteHost(currentData._id);
+
+      toast("Host deleted");
+
+      handleConfirm();
+      setCurrentData({});
+      getData();
+    } catch (error) {
+      toast(error.message);
+    }
+
+    setLoading(false);
+  };
+
+  return (
+    <>
+      <Head>
+        <title>{"Hosts"}</title>
+      </Head>
+      <Box>
+        {!loading ? (
+          <Table
+            table="host"
+            data={hosts}
+            addText={"Add host"}
+            add={() => {
+              setCurrentData(null);
+              handleDialog();
+            }}
+            clk={(data) => {
+              setCurrentData(data);
+              handleDialog();
+            }}
+            del={(data) => {
+              setCurrentData(data);
+              handleConfirm();
+            }}
+          />
+        ) : (
+          <Loading />
+        )}
+      </Box>
+
+      <Dialog
+        open={dialogOpen}
+        onClose={handleDialog}
+        PaperProps={{ sx: { borderRadius: "20px" } }}
+      >
+        <DialogTitle>{"Host"}</DialogTitle>
+        <DialogContent>
+          <HostForm
+            currentData={currentData}
+            getData={getData}
+            handleClose={handleDialog}
+            loading={loading}
+            setLoading={setLoading}
+            updateMode={currentData}
+          />
+        </DialogContent>
+      </Dialog>
+
+      <Confirm
+        onConfirm={deleteData}
+        isOpen={confirmOpen}
+        handleOpen={handleConfirm}
+      />
+    </>
+  );
+};
+
+export default Index;
