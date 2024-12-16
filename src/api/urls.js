6a543b64f8b5d5b44f05f3b718e324366255fe48
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2024-12-16 11:47:39 +0330

    I love backend

---
+++ src/api/urls.js
@@ -0,0 +1,11 @@
+const urls = {
+  auth: {
+    login: "auth/login",
+    register: "auth/register",
+  },
+  permission: "permissions",
+  role: "roles",
+  user: "users",
+};
+
+export default urls;
