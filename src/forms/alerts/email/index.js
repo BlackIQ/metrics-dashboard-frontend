0871a6873d5b0b7661a693b919cabf67c48ec8c6
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2025-03-15 19:18:36 +0330

    Alerts of Telegram and Email form

---
+++ src/forms/alerts/email/index.js
@@ -0,0 +1,77 @@
+// Components
+import { Form } from "@/components";
+
+// Hooks
+import { useToast } from "@/hooks";
+
+// APSs
+import {
+  createOne as createAlert,
+  updateOne as updateAlert,
+} from "@/api/services/alerts";
+
+const AlertEmailForm = ({
+  currentData,
+  updateMode,
+  setLoading,
+  getData,
+  loading,
+  handleClose,
+}) => {
+  const toast = useToast();
+
+  const addData = async (callback) => {
+    setLoading(true);
+
+    const data = { type: "email", config: callback };
+
+    try {
+      await createAlert(data);
+
+      toast("Email alert created");
+      handleClose();
+
+      getData();
+    } catch (error) {
+      toast(error.message);
+    }
+
+    setLoading(false);
+  };
+
+  const updateData = async (callback) => {
+    setLoading(true);
+
+    const data = { config: callback };
+
+    try {
+      await updateAlert(currentData._id, data);
+
+      toast("Email alert updated");
+      handleClose();
+
+      getData();
+    } catch (error) {
+      toast(error.message);
+    }
+
+    setLoading(false);
+  };
+
+  return (
+    <Form
+      name="alertEmail"
+      callback={updateMode !== "non-exists" ? updateData : addData}
+      disables={[]}
+      btnStyle={{
+        fullWidth: false,
+        disabled: loading,
+        color: "primary",
+      }}
+      def={updateMode !== "non-exists" ? { ...currentData.config } : {}}
+      button={updateMode !== "non-exists" ? "Update" : "Create"}
+    />
+  );
+};
+
+export default AlertEmailForm;
