2ac9ff4dd99d282fc439cee1f3688e00ac217795
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2025-03-15 19:17:31 +0330

    Implement more methods

---
+++ src/api/services/alerts/index.js
@@ -0,0 +1,44 @@
+import API from "@/api";
+import URLs from "@/api/urls";
+
+const { alerts } = URLs;
+
+export const listUserAlerts = async () => {
+  try {
+    const response = await API.get(alerts);
+
+    return Promise.resolve(response.data);
+  } catch (error) {
+    return Promise.reject(error.response.data);
+  }
+};
+
+export const createOne = async (data) => {
+  try {
+    const response = await API.post(alerts, data);
+
+    return Promise.resolve(response.data);
+  } catch (error) {
+    return Promise.reject(error.response.data);
+  }
+};
+
+export const deleteOne = async (id) => {
+  try {
+    const response = await API.delete(`${alerts}/${id}`);
+
+    return Promise.resolve(response.data);
+  } catch (error) {
+    return Promise.reject(error.response.data);
+  }
+};
+
+export const updateOne = async (id, data) => {
+  try {
+    const response = await API.patch(`${alerts}/${id}`, data);
+
+    return Promise.resolve(response.data);
+  } catch (error) {
+    return Promise.reject(error.response.data);
+  }
+};
