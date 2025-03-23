54a9b73b0cb5f0c95a35ee9dd865854c5d5d7669
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2025-03-23 13:50:38 +0330

    Add new services

--- src/api/services/auth/index.js
+++ src/api/services/auth/index.js
@@ -3,19 +3,11 @@ import URLs from "@/api/urls";
 
 const { auth } = URLs;
 
+// Authentication
+
 export const loginAccount = async (data) => {
   try {
     const response = await API.post(`${auth}/login`, data);
-
-    return Promise.resolve(response.data);
-  } catch (error) {
-    return Promise.reject(error.response.data);
-  }
-};
-
-export const logoutAccount = async () => {
-  try {
-    const response = await API.post(`${auth}/logout`);
 
     return Promise.resolve(response.data);
   } catch (error) {
@@ -33,6 +25,20 @@ export const registerAccount = async (data) => {
   }
 };
 
+// Logout
+
+export const logoutAccount = async () => {
+  try {
+    const response = await API.post(`${auth}/logout`);
+
+    return Promise.resolve(response.data);
+  } catch (error) {
+    return Promise.reject(error.response.data);
+  }
+};
+
+// Confirm Account
+
 export const confirmEmail = async (rayid) => {
   try {
     const response = await API.post(`${auth}/confirm`, {
@@ -49,6 +55,59 @@ export const resendConfirmEmail = async (email) => {
   try {
     const response = await API.post(`${auth}/resend-confirm`, {
       email,
+    });
+
+    return Promise.resolve(response.data);
+  } catch (error) {
+    return Promise.reject(error.response.data);
+  }
+};
+
+// Change Email
+
+export const changeEmail = async (newEmail) => {
+  try {
+    const response = await API.post(`${auth}/change-email`, {
+      newEmail,
+    });
+
+    return Promise.resolve(response.data);
+  } catch (error) {
+    return Promise.reject(error.response.data);
+  }
+};
+
+export const confirmChangeEmail = async (rayid) => {
+  try {
+    const response = await API.post(`${auth}/confirm-email-change`, {
+      rayid,
+    });
+
+    return Promise.resolve(response.data);
+  } catch (error) {
+    return Promise.reject(error.response.data);
+  }
+};
+
+// Change Password
+
+export const forgotPassword = async (email) => {
+  try {
+    const response = await API.post(`${auth}/forgot-password`, {
+      email,
+    });
+
+    return Promise.resolve(response.data);
+  } catch (error) {
+    return Promise.reject(error.response.data);
+  }
+};
+
+export const resetPassword = async (rayid, newPassword) => {
+  try {
+    const response = await API.post(`${auth}/reset-password`, {
+      rayid,
+      newPassword,
     });
 
     return Promise.resolve(response.data);
