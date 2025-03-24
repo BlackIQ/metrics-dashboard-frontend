7b49171e7c85be0bfec80f1c1ef16e3e37dc8dc4
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2025-03-24 18:47:43 +0330

    New chart colors

---
+++ src/theme/dashboard-colors.js
@@ -0,0 +1,12 @@
+export const dashboardColors = [
+  "#d62728", // Red
+  "#2ca02c", // Green
+  "#1f77b4", // Blue
+  "#ff7f0e", // Amber
+  "#8c564b", // Orange
+  "#bcbd22", // Yello
+  "#17becf", // Teal
+  "#9467bd", // Purple
+  "#e377c2", // Ping
+  "#7f7f7f", // Gray
+];
