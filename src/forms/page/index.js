b89615c04990f3ba964867f62bc6d5fa0ce8e1f5
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2025-03-26 16:09:44 +0330

    Add form of page

---
+++ src/forms/page/index.js
@@ -0,0 +1,70 @@
+// - - - - - Components - - - - -
+import { Form } from "@/components";
+
+// - - - - - Hooks - - - - -
+import { useToast } from "@/hooks";
+
+// - - - - - API - - - - -
+import { createPage, updatePage } from "@/api/services/page";
+
+const PageForm = ({
+  currentData,
+  updateMode,
+  setLoading,
+  getData,
+  loading,
+  handleClose,
+}) => {
+  const toast = useToast();
+
+  const addData = async (callback) => {
+    setLoading(true);
+
+    try {
+      await createPage(callback);
+
+      toast("Page created");
+      handleClose();
+
+      getData();
+    } catch (error) {
+      toast(error.message);
+    }
+
+    setLoading(false);
+  };
+
+  const updateData = async (data) => {
+    setLoading(true);
+
+    try {
+      await updatePage(currentData._id, data);
+
+      toast("Page updated");
+      handleClose();
+
+      getData();
+    } catch (error) {
+      toast(error.message);
+    }
+
+    setLoading(false);
+  };
+
+  return (
+    <Form
+      name="page"
+      callback={updateMode ? updateData : addData}
+      disables={[]}
+      btnStyle={{
+        fullWidth: false,
+        disabled: loading,
+        color: "primary",
+      }}
+      def={updateMode ? currentData : {}}
+      button={updateMode ? "Update" : "Create"}
+    />
+  );
+};
+
+export default PageForm;
