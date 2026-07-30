01263ec796490aea211eaa14ff78bd065ed39bbc
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-30 16:26:34 +0330

    Compact

--- src/api/services/group/index.ts
+++ src/api/services/group/index.ts
@@ -1,25 +1,21 @@
 import { API } from "@/api";
 import URLs from "@/api/urls";
-
 import { GroupCreate, GroupUpdate, GroupRead } from "@/types/group";
 
 const { group } = URLs;
 
 export const allGroups = async (): Promise<GroupRead[]> => {
   const response = await API.get<GroupRead[]>(group);
-
   return response.data;
 };
 
 export const getGroup = async (id: string): Promise<GroupRead> => {
   const response = await API.get<GroupRead>(`${group}/${id}`);
-
   return response.data;
 };
 
 export const createGroup = async (data: GroupCreate): Promise<GroupRead> => {
   const response = await API.post<GroupRead>(group, data);
-
   return response.data;
 };
 
@@ -28,7 +24,6 @@ export const updateGroup = async (
   data: GroupUpdate,
 ): Promise<GroupRead> => {
   const response = await API.patch<GroupRead>(`${group}/${id}`, data);
-
   return response.data;
 };
 
