68251fc53eba34d300207039cb6eb8ebb33fc5aa
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-30 23:50:17 +0330

    Firebase util

---
+++ src/utils/firebase.util.ts
@@ -0,0 +1,24 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+import firebaseSettings from "@/core/firebase/firebase.config";
+
+const firebaseConfig = {
+  apiKey: firebaseSettings.apiKey,
+  authDomain: firebaseSettings.authDomain,
+  projectId: firebaseSettings.projectId,
+  storageBucket: firebaseSettings.storageBucket,
+  messagingSenderId: firebaseSettings.messagingSenderId,
+  appId: firebaseSettings.appId,
+};
+
+const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+
+export const signInWithGoogle = async (): Promise<string> => {
+  const result = await signInWithPopup(auth, googleProvider);
+  const idToken = await result.user.getIdToken();
+
+  return idToken;
+};
