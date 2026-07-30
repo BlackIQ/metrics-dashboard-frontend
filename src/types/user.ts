3355b6f04ebe7e3849b5572912cb80494eb6cc15
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-30 17:37:53 +0330

    New payload types

--- src/types/user.ts
+++ src/types/user.ts
@@ -8,3 +8,18 @@ export interface UserProfile {
   created_at: string;
   updated_at: string;
 }
+
+export interface UpdateProfilePayload {
+  first_name: string;
+  last_name: string;
+}
+
+export interface UpdatePasswordPayload {
+  current_password: string;
+  new_password: string;
+  confirm_password: string;
+}
+
+export interface UpdateEmailPayload {
+  email: string;
+}
