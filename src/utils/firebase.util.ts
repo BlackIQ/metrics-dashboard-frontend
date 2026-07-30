d6684c7b6927da6d720f15b17f0afee56382a520
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-31 02:07:55 +0330

    Add methods for GitHub and Facebook

--- src/utils/firebase.util.ts
+++ src/utils/firebase.util.ts
@@ -1,5 +1,11 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  FacebookAuthProvider,
+  GithubAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
 
 import firebaseSettings from "@/core/firebase/firebase.config";
 
@@ -17,9 +23,25 @@ const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 const googleProvider = new GoogleAuthProvider();
+const facebookProvider = new FacebookAuthProvider();
+const githubProvider = new GithubAuthProvider();
 
 export const signInWithGoogle = async (): Promise<string> => {
   const result = await signInWithPopup(auth, googleProvider);
+  const idToken = await result.user.getIdToken();
+
+  return idToken;
+};
+
+export const signInWithFacebook = async (): Promise<string> => {
+  const result = await signInWithPopup(auth, facebookProvider);
+  const idToken = await result.user.getIdToken();
+
+  return idToken;
+};
+
+export const signInWithGitHub = async (): Promise<string> => {
+  const result = await signInWithPopup(auth, githubProvider);
   const idToken = await result.user.getIdToken();
 
   return idToken;
